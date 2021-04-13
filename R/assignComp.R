a92a9a386d4ead58a8ed89d205b51d8cced33f63
Evert Heyrman <evert.heyrman@ilvo.vlaanderen.be>
2021-04-13 13:27:07 +0200

    assignComp function adapted and simplified

--- R/assignComp.R
+++ R/assignComp.R
@@ -2,32 +2,30 @@
 #'
 #' @param data data.table
 #' @param beh character vector
+#' @param nPC numeric
 #'
 #' @return data.table
 #' @export
+#'
+#' @import groupdata2
 #'
 #' @examples
 #' animalsInTrial <- selectTrialAnimals(biggen,72,72,5.4,9.5)[[1]]
 #' animalsInTrial <- assignPens(animalsInTrial,6,0.1)
 #' animalsInTrial <- assignTreatment(animalsInTrial,c("Wit","Groen","Rood","Geel"))
 #' animalsInTrial <- assignComp(animalsInTrial,c("Wit","Groen","Rood","Geel"))
-assignComp <- function(data,beh){
+assignComp <- function(data,beh,nPC=8){
   Sex <- Gew_klasse <- Beh <- Hok <- Comp <- NULL #To prevent 'no visible binding' note according to https://cran.r-project.org/web/packages/data.table/vignettes/datatable-importing.html
   comp <- data[,list(unique(Sex),unique(Gew_klasse),unique(Beh)),Hok]
   names(comp) <- c("Hok","Sex","Gew_klasse","Beh")
-  x <- nrow(comp) / 8
+  x <- nrow(comp) / nPC
 
-  compNewB<-data.frame()
+  chi.p <- 0
+while (chi.p<0.1) { compNewB<-data.frame()
   for (i in 1:length(beh)) {
     compTempB <- comp[Beh==beh[i]&Sex=="B"]
 
-    count <- 0
-    while (count == 0){
-      compTempB$Comp <- sample(c(rep(1:x,(nrow(compTempB)/x))),replace = F)
-      if (max(compTempB[,sum(duplicated(Gew_klasse)),Comp]$V1) == 0 ){
-        count <- count + 1
-      }
-    }
+    compTempB <- fold(compTempB, k = x,method="n_dist")
 
     compNewB <- rbind(compNewB,compTempB)
   }
@@ -36,13 +34,7 @@ assignComp <- function(data,beh){
   for (i in 1:length(beh)) {
     compTempZ <- comp[Beh==beh[i]&Sex=="Z"]
 
-    count <- 0
-    while (count == 0){
-      compTempZ$Comp <- sample(c(rep(1:x,(nrow(compTempZ)/x))),replace = F)
-      if (max(compTempZ[,sum(duplicated(Gew_klasse)),Comp]$V1) == 0){
-        count <- count + 1
-      }
-    }
+    compTempZ <- fold(compTempZ, k = x,method="n_dist")
 
     compNewZ <- rbind(compNewZ,compTempZ)
   }
@@ -52,8 +44,12 @@ assignComp <- function(data,beh){
   tempAIT <- data
   tempAIT$Comp <- tempAIT$Hok
   for (i in 1:nrow(tempAIT)){
-    tempAIT$Comp[i] <- compNew$Comp[which(compNew$Hok==tempAIT$Comp[i])]
+    tempAIT$Comp[i] <- compNew$.folds[which(compNew$Hok==tempAIT$Comp[i])]
   }
+
+  chi <- chisq.test(table(tempAIT$Comp,tempAIT$Gew_klasse))
+  chi.p <- chi$p.value
+}
 
   tempAIT <- tempAIT[order(Comp)]
   return(tempAIT)
--- TestScript.R
+++ TestScript.R
@@ -3,8 +3,24 @@
 animalsInTrial <- selectTrialAnimals(biggen,72,72,5.4,9.5)[[1]]
 min(animalsInTrial$Speen_gew)
 animalsInTrial <- assignPens(animalsInTrial,6,0.1,nWC=3)
-animalsInTrial <- assignTreatment(animalsInTrial,c("Wit","Geel"))
-animalsInTrial <- assignComp(animalsInTrial,c("Wit","Geel"))
+animalsInTrial <- assignTreatment(animalsInTrial,c("Wit","Geel","Groen","Rood"))
+animalsInTrial <- assignComp(animalsInTrial,c("Wit","Geel","Groen","Rood"))
 table(animalsInTrial$Comp,animalsInTrial$Gew_klasse,animalsInTrial$Beh)
 
+group_by
 
+comp<-data.table(Pen=c(2,3,4,5,16,18,20,22,9,10,11,12,13,14,15,19,1,6,7,8,17,21,23,24),Sex=rep(c("B","B","B","B","Z","Z","Z","Z"),3),Weight=c("L","L","L","M","L","M","M","Z","Z","Z","Z","Z","L","L","L","M","L","M","M","M","M","Z","Z","Z"),Treatment=c("G","W","W","G","W","G","W","G","G","G","W","W","G","G","W","W","G","G","W","W","G","G","W","W"))
+
+comp <- comp[order(Sex,Treatment,Weight)]
+
+test <-fold(comp[Treatment=="W"&Sex=="B"][c(1,3,5)], k = 3,method="n_dist")
+
+table(test$.folds,test$Weight)
+table(test$.folds,test$Treatment)
+table(test$.folds,test$Sex)
+
+table(animalsInTrial$Comp,animalsInTrial$Gew_klasse)
+chi<- chisq.test(table(animalsInTrial$Comp,animalsInTrial$Gew_klasse))
+chi$p.value
+table(animalsInTrial$Comp,animalsInTrial$Beh)
+table(animalsInTrial$Comp,animalsInTrial$Sex)
